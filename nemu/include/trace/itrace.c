ef6f0a92be214b27749e23a4c07346147adfd7b0
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-05-10 14:24:19 +0800

    nemu: init for 4 tracers

--- nemu/include/trace/itrace.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "trace.h"
-
-void itrace_init(){
-    FILE *itrace_file = fopen("$NEMU_HOME/trace/itrace.txt", "w+");
-    return;
-}
--- nemu/include/trace/trace.h
+++ nemu/include/trace/trace.h
@@ -2,26 +2,51 @@
 #include<stdio.h>
 
 void itrace_init(){
-    remove("$NEMU_HOME/itrace.txt"); // So previous traces will not be recorded
-    FILE *itrace_file = fopen("$NEMU_HOME/itrace.txt", "w+");
-    fputs("NEMU itrace records\n", itrace_file);
-    //fwrite("NEMU itrace records", strlen("NEMU itrace records"), 1, itrace_file);
-    //fwrite("This file contains the pc, inst and diassembly of each instruction exected by NEMU", strlen("This file contains the pc, inst and diassembly of each instruction exected by NEMU"), 1, itrace_file);
+    if(remove("$NEMU_HOME/itrace.txt")==0){
+        printf("NEMU removed previous itrace records.\n");
+    } // So previous traces will not be recorded
+    return;
+}
+
+void iringbuf_init(){
+    if(remove("$NEMU_HOME/iringbuf.txt")==0){
+        printf("NEMU removed previous iringbuf records.\n");
+    } // So previous traces will not be recorded
+    return;
+}
+
+void mtrace_init(){
+    if(remove("$NEMU_HOME/mtrace.txt")==0){
+        printf("NEMU removed previous mtrace records.\n");
+    } // So previous traces will not be recorded
+    return;
+}
+
+void ftrace_init(){
+    if(remove("$NEMU_HOME/ftrace.txt")==0){
+        printf("NEMU removed previous ftrace records.\n");
+    } // So previous traces will not be recorded
     return;
 }
 
 void trace_init(bool enable_itrace, bool enable_iringbuf, bool enable_mtrace, bool enable_ftrace){
+    printf("Initialized (Enabled) tracer(s): ");
     if(enable_itrace){
+        printf(" itrace ");
         itrace_init();
     }
     if(enable_iringbuf){
-        //init_iringbuf(64);
+        printf(" iringbuf ");
+        iringbuf_init();
     }
     if(enable_mtrace){
-        //init_mtrace();
+        printf(" mtrace ");
+        mtrace_init();
     }
     if(enable_ftrace){
-        //init_ftrace();
+        printf(" ftrace ");
+        ftrace_init();
     }
+    printf("\n");
     return;
 }
