be2acfea4e1cceaf3a18519dc0eed08d0a57f10d
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-05-14 20:45:40 +0800

    nemu: fix some instructions am-kernels: add a all-in-one test

---
+++ am-kernels/tests/cpu-tests/tests/all-in-one.c
@@ -0,0 +1,23 @@
+#include "trap.h"
+int add_short(short a, short b) {
+	short ans = a + b;
+	return ans;
+}
+
+int add_int(int a, int b) {
+	int ans = a + b;
+	return ans;
+}
+
+int add_long(long a, long b) {
+	long ans = a + b;
+	return ans;
+}
+
+int add_longlong(long long a, long long b) {
+	long long ans = a + b;
+	return ans;
+}
+
+void test_add(){
+}
