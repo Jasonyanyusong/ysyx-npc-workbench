d6ed70e97f2b2d486db87d9aae2e445d1fe3a4e0
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-05-18 17:00:20 +0800

    rtl: changed npc-basic to rtl

--- npc-basic/src/test/scala/NPCBtest.scala
+++
@@ -1,6 +0,0 @@
-package npcb
-import chisel3.stage.ChiselGeneratorAnnotation
-
-object npcbMain extends App{
-    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/npcb"), Seq(ChiselGeneratorAnnotation(() => new NPCB)))
-}
