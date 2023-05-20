9aeaada0af697e7fab1217cb46e939ed46fa708d
jasonyanyusong <jasonyanyusong@outlook.com>
2023-05-20 18:07:08 +0800

    npcb_sim: add parse-args

--- npcb_sim.cpp
+++ npcb_sim.cpp
@@ -98,6 +98,49 @@ void paddr_write(uint64_t addr, int len, uint64_t data) {
 uint64_t mem_read(uint64_t addr, int len)                   {return paddr_read(addr, len);}
 uint64_t mem_write(uint64_t addr, int len, uint64_t val)    {paddr_write(addr, len, val); return val;}
 
+//========== Parse args ==========
+
+static int parse_args(int argc, char *argv[]) {
+    const struct option table[] = {
+        {"batch"    , no_argument      , NULL, 'b'},
+        {"log"      , required_argument, NULL, 'l'},
+        {"diff"     , required_argument, NULL, 'd'},
+        {"port"     , required_argument, NULL, 'p'},
+        {"help"     , no_argument      , NULL, 'h'},
+        {"readelf"  , required_argument, NULL, 'r'},
+        {"readdiasm", required_argument, NULL, 'a'},
+        {0          , 0                , NULL,  0 },
+  };
+  int o;
+    while ( (o = getopt_long(argc, argv, "-bhl:d:p:r:a:", table, NULL)) != -1) {
+        switch (o) {
+            case 'b': sdb_set_batch_mode(); break;
+            case 'p': sscanf(optarg, "%d", &difftest_port); break;
+            case 'l': log_file = optarg; printf("log_file = \"%s\"\n", log_file); break;
+            case 'd': diff_so_file = optarg; printf("diff_so_file = \"%s\"\n", diff_so_file); break;
+            case 'r': elf_file = optarg; printf("elf_file = \"%s\"\n", elf_file); break;
+            case 'a': das_file = optarg; printf("das_file = \"%s\"\n", das_file); break;
+            case 1: img_file = optarg; printf("img_file = \"%s\"\n", img_file); return 0;
+            default:
+                printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+                printf("\t-b,--batch              run with batch mode\n");
+                printf("\t-l,--log=FILE           output log to FILE\n");
+                printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+                printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+                printf("\n");
+                exit(0);
+    }
+  }
+  return 0;
+}
+
+//========== Initialize image ==========
+
+//========== Initialize monitor ==========
+void init_monitor(int argc, char *argv[]){
+    // TODO
+}
+
 //========== Debugger User Interface functions ==========
 
 void sdb_c(){
