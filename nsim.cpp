29c1b7a6fe7efc1f6cb12ba47983a968869b868a
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-05-21 15:18:55 +0800

    nsim: memory subsystem implememted

--- nsim.cpp
+++ nsim.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************************
-* Copyright (c) 2023 Yusong Yan, Beijing No. 101 High School
+* Copyright (c) 2023 Yusong Yan, Beijing No.101 High School
 * Copyright (c) 2023 Yusong Yan, University of Washington - Seattle
 *
 * NSIM is licensed under Mulan PSL v2.
@@ -14,6 +14,11 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
+//========== Macro Configurations ==========
+#define mem_start_addr  0x90000000
+#define mem_end_addr    0x9fffffff
+#define mem_size        mem_end_addr - mem_start_addr + 1
+
 //========== Include Headers ==========
 
 #include "verilated.h"
@@ -27,9 +32,30 @@
 
 //========== List functions that will be used later ==========
 
+//---------- Memory ----------
 uint64_t mem_host_read(void *mem_addr, int mem_length);
+void mem_host_write(void *mem_addr, int mem_length, uint64_t mem_data);
+bool mem_addr_in_bound(uint64_t mem_addr);
+
+uint64_t mem_vaddr_ifetch(uint64_t mem_addr, int mem_length);
+uint64_t mem_vaddr_read(uint64_t mem_addr, int mem_length);
+void mem_vaddr_write(uint64_t mem_addr, int mem_length, uint64_t mem_data);
+
+uint8_t* mem_guest_to_host(uint64_t paddr);
+uint64_t mem_host_to_guest(uint8_t *haddr);
+uint8_t* mem_guest_to_host(uint64_t mem_physical_addr);
+uint64_t mem_host_to_guest(uint8_t *mem_host_addr);
+uint64_t mem_pmem_read(uint64_t mem_addr, int mem_length);
+void mem_pmem_write(uint64_t mem_addr, int mem_length, uint64_t mem_data);
+uint64_t mem_paddr_read(uint64_t mem_addr, int mem_length);
+void mem_paddr_write(uint64_t mem_addr, int mem_length, uint64_t mem_data);
+
+void mem_init_mem();
 
 //========== Memory manipulations ==========
+
+static uint8_t *mem_pmem = NULL;
+
 uint64_t mem_host_read(void *mem_addr, int mem_length){
     switch (mem_length){
         case 1: return *(uint8_t  *)mem_addr;
@@ -38,4 +64,73 @@ uint64_t mem_host_read(void *mem_addr, int mem_length){
         case 8: return *(uint64_t *)mem_addr;
         default: printf("[memory] invalid read length\n"); assert(0); return 0;
     }
+}
+
+void mem_host_write(void *mem_addr, int mem_length, uint64_t mem_data){
+    switch (mem_length){
+        case 1: *(uint8_t  *)mem_addr = mem_data; return;
+        case 2: *(uint16_t *)mem_addr = mem_data; return;
+        case 4: *(uint32_t *)mem_addr = mem_data; return;
+        case 8: *(uint64_t *)mem_addr = mem_data; return;
+        default: printf("[memory] invalid write length\n"); assert(0); return;
+    }
+}
+
+bool mem_addr_in_bound(uint64_t mem_addr){
+    if(!mem_addr - mem_start_addr < mem_size)
+        printf("[memory] address 0x%x out of bound [0x%x,0x%x]\n", mem_addr, mem_start_addr, mem_end_addr);
+    return mem_addr - mem_start_addr < mem_size;
+}
+
+void mem_init_mem(){
+    mem_pmem = (uint8_t *)malloc(mem_size);
+    assert(mem_pmem);
+    printf("[memory] physical memory area [0x%x,0x%x] size 0x%x\n", mem_start_addr, mem_end_addr, mem_size);
+}
+
+uint8_t* mem_guest_to_host(uint64_t paddr) { return mem_pmem + paddr - mem_start_addr; }
+uint64_t mem_host_to_guest(uint8_t *haddr) { return haddr - mem_pmem + mem_start_addr; }
+
+uint64_t mem_pmem_read(uint64_t mem_addr, int mem_length){
+    uint64_t ret = mem_host_read(mem_guest_to_host(mem_addr), mem_length);
+    return ret;
+}
+void mem_pmem_write(uint64_t mem_addr, int mem_length, uint64_t mem_data){
+    mem_host_write(mem_guest_to_host(mem_addr), mem_length, mem_data);
+}
+
+uint64_t mem_paddr_read(uint64_t mem_addr, int mem_length){
+    if(mem_addr_in_bound(mem_addr))
+        return mem_pmem_read(mem_addr, mem_length);
+    // should not reach here!
+    assert(0);
+    return 0;
+}
+
+void mem_paddr_write(uint64_t mem_addr, int mem_length, uint64_t mem_data){
+    if(mem_addr_in_bound(mem_addr))
+        mem_pmem_write(mem_addr, mem_length, mem_data);
+    // should not reach here!
+    assert(0);
+    return;
+}
+
+uint64_t mem_vaddr_ifetch(uint64_t mem_addr, int mem_length){
+    return mem_paddr_read(mem_addr, mem_length);
+}
+
+uint64_t mem_vaddr_read(uint64_t mem_addr, int mem_length){
+    return mem_paddr_read(mem_addr, mem_length);
+}
+
+void mem_vaddr_write(uint64_t mem_addr, int mem_length, uint64_t mem_data){
+    mem_paddr_write(mem_addr, mem_length, mem_data);
+}
+
+
+//========== Main function ==========
+int main(){
+    mem_init_mem();
+    printf("Welcome to riscv64-nsim\n");
+    return 0;
 }
