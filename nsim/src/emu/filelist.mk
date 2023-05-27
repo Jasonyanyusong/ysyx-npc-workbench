fcb33fc013458804058ac666b17d79bbb1420d5e
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-05-27 13:09:53 +0800

    nsim: change directory name for isa related files

--- nsim/src/emu/filelist.mk
+++ nsim/src/emu/filelist.mk
@@ -13,5 +13,5 @@
 # See the Mulan PSL v2 for more details.
 #**************************************************************************************/
 
-INC_PATH += $(NSIM_HOME)/src/isa/$(GUEST_ISA)/include
-DIRS-y += src/isa/$(GUEST_ISA)
+INC_PATH += $(NSIM_HOME)/src/emu/$(GUEST_ISA)/include
+DIRS-y += src/emu/$(GUEST_ISA)
