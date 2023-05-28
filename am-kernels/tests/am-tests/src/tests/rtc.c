c99da4d3df82dc03c40fb4659107be30be4a3fe6
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-05-28 16:16:56 +0800

    am: riscv64-nemu-timer implemented

--- am-kernels/tests/am-tests/src/tests/rtc.c
+++ am-kernels/tests/am-tests/src/tests/rtc.c
@@ -6,7 +6,7 @@ void rtc_test() {
   while (1) {
     while(io_read(AM_TIMER_UPTIME).us / 1000000 < sec) ;
     rtc = io_read(AM_TIMER_RTC);
-    printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
+    printf("%d-%d-%d %d:%d:%d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
     if (sec == 1) {
       printf("%d second).\n", sec);
     } else {
