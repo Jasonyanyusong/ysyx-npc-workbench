bf54e9f1fcd9dfbc0b9d7f479ed35edde6028f9a
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-05-31 15:12:03 +0800

    am-kernels: optimize 4byte signed muldivrem test

--- am-kernels/tests/cpu-tests/tests/4s-muldivrem.c
+++ am-kernels/tests/cpu-tests/tests/4s-muldivrem.c
@@ -2,7 +2,7 @@
 
 int main(){
     signed int ans = 0;
-    for(signed int a = -2147483648; a < 2147483647; a = a + 1){
+    for(signed int a = -2147483648; a < -2147483632; a = a + 1){
         for(signed int b = -2147483648; b < -2147483632; b = b + 1){
             ans = a * b;
         }
@@ -10,7 +10,15 @@ int main(){
             ans = a * b;
         }
     }
-    for(signed int a = -2147483648; a < 2147483647; a = a + 1){
+    for(signed int a = 2147483631; a < 2147483647; a = a + 1){
+        for(signed int b = 2147483631; b < 2147483647; b = b + 1){
+            ans = a * b;
+        }
+        for(signed int b = 0; b < 16; b = b + 1){
+            ans = a * b;
+        }
+    }
+    for(signed int a = -2147483648; a < -2147483632; a = a + 1){
         for(signed int b = -2147483648; b < -2147483632; b = b + 1){
             ans = a / b;
         }
@@ -18,15 +26,15 @@ int main(){
             ans = a / b;
         }
     }
-    for(signed int a = -2147483648; a < 2147483647; a = a + 1){
-        for(signed int b = -2147483648; b < -2147483632; b = b + 1){
-            ans = b / a;
+    for(signed int a = 2147483631; a < 2147483647; a = a + 1){
+        for(signed int b = 2147483631; b < 2147483647; b = b + 1){
+            ans = a / b;
         }
         for(signed int b = 0; b < 16; b = b + 1){
-            ans = b / a;
+            ans = a / b;
         }
     }
-    for(signed int a = -2147483648; a < 2147483647; a = a + 1){
+    for(signed int a = -2147483648; a < -2147483632; a = a + 1){
         for(signed int b = -2147483648; b < -2147483632; b = b + 1){
             ans = a % b;
         }
@@ -34,8 +42,40 @@ int main(){
             ans = a % b;
         }
     }
-    for(signed int a = -2147483648; a < 2147483647; a = a + 1){
+    for(signed int a = 2147483631; a < 2147483647; a = a + 1){
+        for(signed int b = 2147483631; b < 2147483647; b = b + 1){
+            ans = a % b;
+        }
+        for(signed int b = 0; b < 16; b = b + 1){
+            ans = a % b;
+        }
+    }
+    for(signed int a = -2147483648; a < -2147483632; a = a + 1){
         for(signed int b = -2147483648; b < -2147483632; b = b + 1){
+            ans = b / a;
+        }
+        for(signed int b = 0; b < 16; b = b + 1){
+            ans = b / a;
+        }
+    }
+    for(signed int a = 2147483631; a < 2147483647; a = a + 1){
+        for(signed int b = 2147483631; b < 2147483647; b = b + 1){
+            ans = b / a;
+        }
+        for(signed int b = 0; b < 16; b = b + 1){
+            ans = b / a;
+        }
+    }
+    for(signed int a = -2147483648; a < -2147483632; a = a + 1){
+        for(signed int b = -2147483648; b < -2147483632; b = b + 1){
+            ans = b % a;
+        }
+        for(signed int b = 0; b < 16; b = b + 1){
+            ans = b % a;
+        }
+    }
+    for(signed int a = 2147483631; a < 2147483647; a = a + 1){
+        for(signed int b = 2147483631; b < 2147483647; b = b + 1){
             ans = b % a;
         }
         for(signed int b = 0; b < 16; b = b + 1){
