3d5b5388e4a7a078b2421e7ab223adcc8b03a862
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-05-31 09:34:28 +0800

    am-kernels: add 1 byte signed tests

---
+++ am-kernels/tests/cpu-tests/tests/1s-shift.c
@@ -0,0 +1,25 @@
+#include "trap.h"
+
+int main(){
+    signed char ans = 0;
+    for(signed char a = -128; a < 127; a = a + 1){
+        for(signed char b = -128; b < 127; b = b + 1){
+            ans = (unsigned char)a << (unsigned char)b;
+            printf("%d, ", ans);
+        }
+    }
+    for(signed char a = -128; a < 127; a = a + 1){
+        for(signed char b = -128; b < 127; b = b + 1){
+            ans = (unsigned char)a >> (unsigned char)b;
+            printf("%d, ", ans);
+        }
+    }
+    for(signed char a = -128; a < 127; a = a + 1){
+        for(signed char b = -128; b < 127; b = b + 1){
+            ans = (signed char)a >> (signed char)b;
+            printf("%d, ", ans);
+        }
+    }
+    printf("\n");
+    return 0;
+}
