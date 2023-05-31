edacda96806a646e225e2935299c9d4562914a5c
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-05-31 14:51:34 +0800

    am-kernels: fix 4 byte shift so slow

--- am-kernels/tests/cpu-tests/tests/4s-shift.c
+++ am-kernels/tests/cpu-tests/tests/4s-shift.c
@@ -2,17 +2,32 @@
 
 int main(){
     signed int ans = 0;
-    for(signed int a = -2147483648; a < 2147483647; a = a + 1){
+    for(signed int a = -2147483648; a < -2147483632 ; a = a + 1){
         for(signed int b = 0; b < 32; b = b + 1){
             ans = (unsigned int)a << b;
         }
     }
-    for(signed int a = -2147483648; a < 2147483647; a = a + 1){
+    for(signed int a = 2147483631; a < 2147483647; a = a + 1){
+        for(signed int b = 0; b < 32; b = b + 1){
+            ans = (unsigned int)a << b;
+        }
+    }
+    for(signed int a = -2147483648; a < -2147483632 ; a = a + 1){
         for(signed int b = 0; b < 32; b = b + 1){
             ans = (unsigned int)a >> b;
         }
     }
-    for(signed int a = -2147483648; a < 2147483647; a = a + 1){
+    for(signed int a = 2147483631; a < 2147483647; a = a + 1){
+        for(signed int b = 0; b < 32; b = b + 1){
+            ans = (unsigned int)a >> b;
+        }
+    }
+    for(signed int a = -2147483648; a < -2147483632 ; a = a + 1){
+        for(signed int b = 0; b < 32; b = b + 1){
+            ans = (signed int)a >> b;
+        }
+    }
+    for(signed int a = 2147483631; a < 2147483647; a = a + 1){
         for(signed int b = 0; b < 32; b = b + 1){
             ans = (signed int)a >> b;
         }
