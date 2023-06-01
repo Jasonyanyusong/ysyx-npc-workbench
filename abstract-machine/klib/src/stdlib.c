029bc143e276daa64a2aa54de9d9294689119d65
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-06-01 09:46:04 +0800

    am: implement stdlib

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
+#include <unistd.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 static unsigned long int next = 1;
@@ -68,12 +69,25 @@ char* itoa(int num, char* str, int base)
     return str;
 }
 
+char* currentAddr = NULL;
+
 void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  if(currentAddr == NULL){
+    currentAddr =(void *)ROUNDUP(heap.start, 8);
+  }
+  size = (size_t)ROUNDUP(size, 8);
+  char* old = currentAddr;
+  currentAddr += size;
+  assert((uintptr_t)heap.start <= (uintptr_t)currentAddr && (uintptr_t)currentAddr < (uintptr_t)heap.end);
+  for(uint64_t *p = (uint64_t*) old; p!= (uint64_t *)currentAddr; p ++){
+    *p = 0;
+  }
+  return old;
+  
 #endif
   return NULL;
 }
