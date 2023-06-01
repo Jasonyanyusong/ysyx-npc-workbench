b99df35997b124a8d2dc35ea470bf6c147ab1c22
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-06-01 09:08:59 +0800

    am: nemu support keyboard input

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -2,8 +2,10 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
+#define KEYCODE_MASK 0x7FFF
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t keyborad_data = inl(KBD_ADDR);
+  kbd->keydown = keyborad_data & KEYDOWN_MASK;
+  kbd->keycode = keyborad_data & KEYCODE_MASK;
 }
