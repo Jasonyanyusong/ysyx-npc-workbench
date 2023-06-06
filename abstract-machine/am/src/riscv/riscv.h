5906ec42974b41c18d2fcde48b36353053057c5d
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-06-06 17:08:29 +0800

    am: riscv.h add long long read (easy for timer)

--- abstract-machine/am/src/riscv/riscv.h
+++ abstract-machine/am/src/riscv/riscv.h
@@ -6,10 +6,12 @@
 static inline uint8_t  inb(uintptr_t addr) { return *(volatile uint8_t  *)addr; }
 static inline uint16_t inw(uintptr_t addr) { return *(volatile uint16_t *)addr; }
 static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; }
+static inline uint64_t inll(uintptr_t addr) { return *(volatile uint64_t *)addr; }
 
 static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
 static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)addr = data; }
 static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
+static inline void outll(uintptr_t addr, uint64_t data) { *(volatile uint64_t *)addr = data; }
 
 #define PTE_V 0x01
 #define PTE_R 0x02
