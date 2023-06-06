8628be5a1391e9bcdb0dfd5d52fc7de1ae0a4d53
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-06-06 16:13:03 +0800

    am: implement riscv64-npc's RTC timer

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -1,10 +1,21 @@
 #include <am.h>
 
+#define RTC_ADDR 0xa0000048
+
+uint64_t am_start_time = 0;
+
+uint64_t am_get_time(){
+  uint32_t time_low = inl(RTC_ADDR);
+  uint32_t time_high = inl(RTC_ADDR + 4);
+  return (time_high << 32) + time_low;
+}
+
 void __am_timer_init() {
+  am_start_time = am_get_time();
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uptime->us = am_get_time() - am_start_time;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
