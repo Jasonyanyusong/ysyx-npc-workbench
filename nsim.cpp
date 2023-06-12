566ca7e55fa968de4ebc63c4f071fb48588e9270
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-06-12 04:30:43 +0000

    nsim: add RTC Timer to mmio devices

--- nsim.cpp
+++ nsim.cpp
@@ -244,6 +244,13 @@ void device_serial_io_handler(uint32_t offset, int len, bool is_write);
 #define DEVICE_SERIO_CH_OFFSET 0
 static uint8_t *device_serial_base = NULL;
 
+
+void device_rtc_io_handler(uint32_t offset, int len, bool is_write);
+void device_rtc_time_interrupt(); // Not implemented yet
+
+#define DEVICE_RTC_BASE        0xa0000048
+static uint32_t *device_rtc_port_base = NULL;
+
 //typedef uint16_t ioaddr_t;
 
 //---------- Device: Map & MMIO----------
@@ -448,6 +455,31 @@ void device_init_serial(){
     device_add_mmio_map("serial", DEVICE_SERIAL_BASE, device_serial_base, 8, device_serial_io_handler);
     return;
 }
+
+//========== Device: RTC Timer ==========
+
+void device_init_timer(){
+    device_rtc_port_base = (uint32_t *)device_map_new_space(8);
+    device_add_mmio_map("rtc-timer", DEVICE_RTC_BASE, device_rtc_port_base, 8, device_rtc_io_handler);
+    //device_add_alarm_handle(device_rtc_time_interrupt);
+    return;
+}
+
+void device_rtc_io_handler(uint32_t offset, int len, bool is_write){
+    assert(offset == 0 || offset == 4);
+    if(!is_write && offset == 4){
+        uint64_t us = host_timer_get_time();
+        device_rtc_port_base[0] = (uint32_t)us;
+        device_rtc_port_base[1] = us >> 32;
+    }
+    return;
+}
+
+void device_rtc_time_interrupt(){
+    // TODO
+    assert(0);
+    return;
+} // Not implemented yet
 
 //========== Device-Serial ==========
 
