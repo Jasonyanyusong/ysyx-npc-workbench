9ed340d24db01907fb0bbe4f74e71c4a43db343a
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-06-14 06:50:03 +0000

    am: riscv-npc IOS add UART not present

--- abstract-machine/am/src/riscv/npc/ioe.c
+++ abstract-machine/am/src/riscv/npc/ioe.c
@@ -10,6 +10,7 @@ void __am_timer_rtc(AM_TIMER_RTC_T *);
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
+static void __am_uart_config (AM_INPUT_CONFIG_T *cfg) { cfg->present = false; }
 
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
@@ -17,6 +18,7 @@ static void *lut[128] = {
   [AM_TIMER_RTC   ] = __am_timer_rtc,
   [AM_TIMER_UPTIME] = __am_timer_uptime,
   [AM_INPUT_CONFIG] = __am_input_config,
+  [AM_UART_CONFIG ] = __am_uart_config,
   [AM_INPUT_KEYBRD] = __am_input_keybrd,
 };
 
