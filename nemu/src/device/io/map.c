9335aba80d77c753685e47432c6ccc86233b3fba
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-06-16 17:58:41 +0800

    nemu: fix dtrace (have no switch)

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -59,7 +59,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
-  dtrace_write(cpu.pc, map -> name, addr, len, false, ret);
+  IFDEF(CONFIG_DeviceTrace, dtrace_write(cpu.pc, map -> name, addr, len, false, ret));
   return ret;
 }
 
@@ -68,6 +68,6 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
-  dtrace_write(cpu.pc, map -> name, addr, len, true, data);
+  IFDEF(CONFIG_DeviceTrace, dtrace_write(cpu.pc, map -> name, addr, len, true, data));
   invoke_callback(map->callback, offset, len, true);
 }
