fc569ae25117b4c958e0f3361f3a20e00e2f4637
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-06-16 13:37:01 +0800

    nemu: rv64-reg, remove fpr, optimize print csr

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -39,7 +39,7 @@ void isa_display_gpr(){
   return;
 }
 
-void isa_display_fpr(){
+/*void isa_display_fpr(){
   printf("FPR:\n");
   for(int i = 0; i < 32; i = i + 1){
     printf("f%2d = %16lx ", i, cpu.fpr[i]);
@@ -49,23 +49,45 @@ void isa_display_fpr(){
   }
   printf("\n");
   return;
-}
+}*/
 
 void isa_display_csr(){
   printf("CSR:\n");
-  for(int i = 0; i < 4096; i = i + 1){
+  /*for(int i = 0; i < 4096; i = i + 1){
     printf("csr0x%3x = %16lx ", i, cpu.csr[i]);
     if((i + 1) % 8 == 0){
       printf("\n");
     }
-  }
+  }*/
+
+  // M state CSRs
+  printf("(csr[0xF11] MRO) mvendorid  = 0x%lx\n", cpu.csr[0xF11]);
+  printf("(csr[0xF12] MRO) marchid    = 0x%lx\n", cpu.csr[0xF12]);
+  printf("(csr[0xF13] MRO) mimpid     = 0x%lx\n", cpu.csr[0xF13]);
+  printf("(csr[0xF14] MRO) mhartid    = 0x%lx\n", cpu.csr[0xF14]);
+  printf("(csr[0xF15] MRO) mconfigptr = 0x%lx\n", cpu.csr[0xF15]);
+  printf("(csr[0x300] MRW) mstatus    = 0x%lx\n", cpu.csr[0x300]);
+  printf("(csr[0x301] MRW) misa       = 0x%lx\n", cpu.csr[0x301]);
+  printf("(csr[0x302] MRW) medeleg    = 0x%lx\n", cpu.csr[0x302]);
+  printf("(csr[0x303] MRW) mideleg    = 0x%lx\n", cpu.csr[0x303]);
+  printf("(csr[0x304] MRW) mie        = 0x%lx\n", cpu.csr[0x304]);
+  printf("(csr[0x305] MRW) mtvec      = 0x%lx\n", cpu.csr[0x305]);
+  printf("(csr[0x306] MRW) mcounteren = 0x%lx\n", cpu.csr[0x306]);
+  printf("(csr[0x340] MRW) mscratch   = 0x%lx\n", cpu.csr[0x340]);
+  printf("(csr[0x341] MRW) mepc       = 0x%lx\n", cpu.csr[0x341]);
+  printf("(csr[0x342] MRW) mcause     = 0x%lx\n", cpu.csr[0x342]);
+  printf("(csr[0x343] MRW) mtval      = 0x%lx\n", cpu.csr[0x343]);
+  printf("(csr[0x344] MRW) mscratch   = 0x%lx\n", cpu.csr[0x344]);
+  printf("(csr[0x34A] MRW) mtinst     = 0x%lx\n", cpu.csr[0x34A]);
+  printf("(csr[0x34B] MRW) mtval2     = 0x%lx\n", cpu.csr[0x34B]);
+
   printf("\n");
   return;
 }
 
 void isa_reg_display() {
   isa_display_gpr();
-  isa_display_fpr();
+  //isa_display_fpr();
   isa_display_csr();
   return;
 }
