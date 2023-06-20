3254921a9567aa36e6553114e644113bf479a354
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-06-20 15:07:21 +0800

    npca: sepreate project

---
+++ npca/src/test/scala/npcatest.scala
@@ -0,0 +1,6 @@
+package npca
+import chisel3.stage.ChiselGeneratorAnnotation
+
+object npcMain extends App{
+    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/npca"), Seq(ChiselGeneratorAnnotation(() => new npca)))
+}
