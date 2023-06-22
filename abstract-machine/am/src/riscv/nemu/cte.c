7c43d837da482465c4c1a6d0a96efb42b80ba038
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-06-22 14:36:23 +0800

    am: rv64-nemu mepc bug fixed

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -7,12 +7,24 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
-    switch (c->mcause) {
+    /*switch (c->mcause) {
       case -1: ev.event = EVENT_YIELD; break;
       default: ev.event = EVENT_ERROR; break;
+    }*/
+
+    if(c -> mcause == 11) {
+      if(c -> GPR1 == -1){
+        ev.event  = EVENT_YIELD;
+      }
+      else{
+        ev.event = EVENT_ERROR;
+      }
     }
 
     c = user_handler(ev, c);
+
+    c -> mepc = c -> mepc + 4; // set error pc to the next inst using software
+
     assert(c != NULL);
   }
 
