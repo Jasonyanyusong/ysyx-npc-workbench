5788ea0bdeae9fc542fc7730933803d865bc2745
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-06-27 03:34:05 +0000

    npcb: fix test scala file privious change is fix npca npc scala

--- npca/src/test/scala/npcatest.scala
+++ npca/src/test/scala/npcatest.scala
@@ -1,6 +1,6 @@
 package npca
 import chisel3.stage.ChiselGeneratorAnnotation
 
-object npcaMain extends App{
-    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/npca"), Seq(ChiselGeneratorAnnotation(() => new npca)))
+object npcMain extends App{
+    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/npc"), Seq(ChiselGeneratorAnnotation(() => new npc)))
 }
