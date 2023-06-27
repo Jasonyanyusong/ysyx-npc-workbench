193f4c257a7979083ea5d67dba72829de3bb5aa9
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-06-27 03:33:43 +0000

    npcb: fix test scala file

--- npcb/src/test/scala/npcbtest.scala
+++ npcb/src/test/scala/npcbtest.scala
@@ -1,6 +1,6 @@
 package npcb
 import chisel3.stage.ChiselGeneratorAnnotation
 
-object npcbMain extends App{
-    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/npcb"), Seq(ChiselGeneratorAnnotation(() => new npcb)))
+object npcMain extends App{
+    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/npc"), Seq(ChiselGeneratorAnnotation(() => new npc)))
 }
