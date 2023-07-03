7359c38e3eec4dcfc12236ca8fe067d6928749a1
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-07-03 21:34:38 +0800

    nanos-lite: mepc + 4 only for EVENT_YIELD now

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,11 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case EVENT_YIELD: Log("find EVENT_YIELD"); break;
+    case EVENT_YIELD: {
+      Log("find EVENT_YIELD"); 
+      c -> mepc = c -> mepc + 4; // set error pc to the next inst using software
+      break;
+    }
     default: panic("Unhandled event ID = %d", e.event);
   }
 
