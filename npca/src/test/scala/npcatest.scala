ff38754457019c8850fb475965b60b7d466162e2
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-07-03 21:20:35 +0800

    npca: generater will also generate partial code

--- npca/src/test/scala/npcatest.scala
+++ npca/src/test/scala/npcatest.scala
@@ -3,4 +3,10 @@ import chisel3.stage.ChiselGeneratorAnnotation
 
 object npcMain extends App{
     (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/npc"), Seq(ChiselGeneratorAnnotation(() => new npc)))
+
+    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/parts"), Seq(ChiselGeneratorAnnotation(() => new IFU)))
+    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/parts"), Seq(ChiselGeneratorAnnotation(() => new IDU)))
+    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/parts"), Seq(ChiselGeneratorAnnotation(() => new EXU)))
+    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/parts"), Seq(ChiselGeneratorAnnotation(() => new LSU)))
+    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/parts"), Seq(ChiselGeneratorAnnotation(() => new WBU)))
 }
