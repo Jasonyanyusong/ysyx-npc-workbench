cc848ebdf3cff28dff4e9578d47085fa0dbfa7cb
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-07-05 16:39:48 +0800

    ysyx: remove npcs

--- npcs/src/test/scala/npcstest.scala
+++
@@ -1,6 +0,0 @@
-package npcs
-import chisel3.stage.ChiselGeneratorAnnotation
-
-object npcsMain extends App{
-    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/npcs"), Seq(ChiselGeneratorAnnotation(() => new npcs)))
-}
