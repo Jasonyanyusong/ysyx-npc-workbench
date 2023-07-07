ec7fa80ed6dfdcfc3c98d32710eff45954f94964
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-07-07 16:34:44 +0800

    nanos-lite: support SYS_yield

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,41 @@
 #include <common.h>
 #include "syscall.h"
+
+enum {
+  SYS_exit,
+  SYS_yield,
+  SYS_open,
+  SYS_read,
+  SYS_write,
+  SYS_kill,
+  SYS_getpid,
+  SYS_close,
+  SYS_lseek,
+  SYS_brk,
+  SYS_fstat,
+  SYS_time,
+  SYS_signal,
+  SYS_execve,
+  SYS_fork,
+  SYS_link,
+  SYS_unlink,
+  SYS_wait,
+  SYS_times,
+  SYS_gettimeofday
+};
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+    case SYS_yield: sys_yield(c); break;
+    default: panic("Unhandled syscall ID = %d", a[0]); break;
   }
+}
+
+void sys_yield(Context *c){
+  Log("Do SYSCALL: YIELD");
+  yield();
+  c->GPRx = 0;
 }
