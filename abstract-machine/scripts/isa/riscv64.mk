24e2ada6628561b09dd514a9fd20e82aa0ce6268
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-07-07 11:25:25 +0800

    am: raise rv64's compile time optimize level

--- abstract-machine/scripts/isa/riscv64.mk
+++ abstract-machine/scripts/isa/riscv64.mk
@@ -1,5 +1,5 @@
 CROSS_COMPILE := riscv64-linux-gnu-
 COMMON_FLAGS  := -fno-pic -march=rv64g -mcmodel=medany -mstrict-align
-CFLAGS        += $(COMMON_FLAGS) -static -O0
-ASFLAGS       += $(COMMON_FLAGS) -O0
+CFLAGS        += $(COMMON_FLAGS) -static -O3
+ASFLAGS       += $(COMMON_FLAGS) -O3
 LDFLAGS       += -melf64lriscv
