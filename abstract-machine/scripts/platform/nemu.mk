4c0fd9087917a8198af1f816881e523734f8b022
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-07-08 20:38:44 +0800

    am: send nanos/navy ELF to nemu

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -14,6 +14,7 @@ LDFLAGS   += --gc-sections -e _start
 NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 NEMUFLAGS += -r $(IMAGE).elf
 NEMUFLAGS += -a $(IMAGE).txt
+NEMUFLAGS += -n $(NANOS_HOME)/build/ramdisk.img
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
