78ba3a1bbbc3e882c7815cec900ee0bce67aa2c4
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-07-09 12:53:43 +0800

    nanos-lite: add open_offset in fs

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -9,6 +9,7 @@ typedef struct {
   size_t disk_offset;
   ReadFn read;
   WriteFn write;
+  size_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
@@ -25,9 +26,9 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write, 0},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write, 0},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write, 0},
 #include "files.h"
 };
 
