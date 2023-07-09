8dfaeff705bd8f75e9de4d31cad4aff95865b040
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-07-09 16:39:14 +0800

    navy-apps: add more sys_calls

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -60,8 +60,9 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  _exit(SYS_open);
-  return 0;
+  //_exit(SYS_open);
+  //return 0;
+  return _syscall_(SYS_open, (void *)path, flags, mode);
 }
 
 int _write(int fd, void *buf, size_t count) {
@@ -80,18 +81,21 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  _exit(SYS_read);
-  return 0;
+  //_exit(SYS_read);
+  //return 0;
+  return _syscall_(SYS_read, fd, (uintptr_t)buf, count);
 }
 
 int _close(int fd) {
-  _exit(SYS_close);
-  return 0;
+  //_exit(SYS_close);
+  //return 0;
+  return _syscall_(SYS_close, fd, 0, 0);
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _exit(SYS_lseek);
-  return 0;
+  //_exit(SYS_lseek);
+  //return 0;
+  return _syscall_(SYS_lseek, fd, offset, whence);
 }
 
 int _gettimeofday(struct timeval *tv, struct timezone *tz) {
