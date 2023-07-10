da059e08a1eb7d68203cab68d55d480c05ca28a7
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-07-10 16:11:59 +0800

    navy-apps: timer-test add NDL time test

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <stdbool.h>
+#include "NDL.h"
 
 int main(){
     struct timeval timeNow;
     long long timePassed = 0;
     timeNow.tv_sec = 0;
     timeNow.tv_usec = 0;
+
+    long long NDL_sec = 0;
     while(true){
         while(timeNow.tv_sec < timePassed + 1){
             gettimeofday(&timeNow, NULL);
         }
-        printf("[timer-test] 1 second passed, tv_sec = %d, tv_usec = %d\n", timeNow.tv_sec, timeNow.tv_usec);
+        printf("[timer-test: gettimeofday] %d second passed, tv_sec = %d, tv_usec = %d\n", timeNow.tv_sec, timeNow.tv_sec, timeNow.tv_usec);
         timePassed = timePassed + 1;
+
+        while(NDL_sec < timePassed + 1){
+            NDL_sec = NDL_GetTicks() / 1000;
+            //printf("NDL sec = %d cond = %d\n", NDL_sec, timePassed + 1);
+        }
+        printf("[timer-test: NDL_GetTicks] %d second passed, NDL_GetTicks = %d\n", NDL_sec, NDL_GetTicks()); 
+        timePassed = timePassed + 1;
+
+        if(timePassed == 120){
+            break;
+        }
     }
     return 0;
 }
