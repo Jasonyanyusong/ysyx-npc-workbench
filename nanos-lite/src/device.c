f2ba1367bcb0f4ca30f51058479ec2da62f75531
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-07-11 06:33:29 +0800

    nanos-lite: device add read display info function

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -51,7 +51,10 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  return 0;
+  // TODO: read from am
+  AM_GPU_CONFIG_T GPUConfig = io_read(AM_GPU_CONFIG);
+  sprintf(buf, "WIDTH:%d\nHEIGHT:%d\n",GPUConfig.width, GPUConfig.height);
+  return strlen(buf);
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
