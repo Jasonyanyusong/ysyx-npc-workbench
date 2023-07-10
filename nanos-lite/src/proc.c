9150d71b1680005102dad64151190494a44581d0
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-07-11 06:33:08 +0800

    navy-apps: loader load bmp-test

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,7 +25,9 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/event-test");
+
+  naive_uload(NULL, "/bin/bmp-test");
+  //naive_uload(NULL, "/bin/event-test");
   //naive_uload(NULL, "/bin/timer-test");
   //naive_uload(NULL, "/bin/file-test");
   //naive_uload(NULL, "/bin/dummy");
