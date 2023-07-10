18e04db2481a72f0a820d0a947a646df07c0f123
Jasonyanyusong <Jasonyanyusong@outlook.com>
2023-07-10 16:11:43 +0800

    navy-apps: NDL impment GetTicks

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -3,13 +3,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 static int evtdev = -1;
 static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 
 uint32_t NDL_GetTicks() {
-  return 0;
+  struct timeval getTime;
+  gettimeofday(&getTime, NULL);
+  //printf("gettime: %d, return: %d\n", getTime.tv_usec, (getTime.tv_usec / 1000));
+  return (getTime.tv_usec / 1000);
+  //return 0;
 }
 
 int NDL_PollEvent(char *buf, int len) {
