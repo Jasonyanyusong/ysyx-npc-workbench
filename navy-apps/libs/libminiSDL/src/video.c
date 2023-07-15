d0a4d66ea22998ee1edd839a3673ee6e607161eb
Jasonyanyusong <jasonyys@uw.edu>
2023-07-15 18:18:42 +0800

    navy-apps: libminiSDL add UpdateRect func

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   assert(dst && src);
@@ -13,6 +14,10 @@ void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
 }
 
 void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
+  printf("[miniSDL] x = %d, y = %d, w = %d, h = %d\n", x, y, w, h);
+  //assert(0);
+  NDL_DrawRect((uint32_t *)s->pixels, x, y, s->w, s->h);
+  printf("[miniSDL] x = %d, y = %d, s->w = %d, s->h = %d\n", x, y, s->w, s->h);
 }
 
 // APIs below are already implemented.
