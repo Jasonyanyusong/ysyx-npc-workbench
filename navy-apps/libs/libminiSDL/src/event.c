2d069d921b43378ecd3a9f8d96ccb1553cee1b3d
Jasonyanyusong <jasonyys@uw.edu>
2023-07-15 21:38:01 +0800

    navy-apps: miniSDL implement SDL_WaitEvent

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -1,5 +1,7 @@
 #include <NDL.h>
 #include <SDL.h>
+#include <assert.h>
+#include <string.h>
 
 #define keyname(k) #k,
 
@@ -17,6 +19,25 @@ int SDL_PollEvent(SDL_Event *ev) {
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
+  char buf[64];
+  if (NDL_PollEvent(buf, sizeof(buf))) {
+    printf("receive event: %s\n", buf);
+    char keyType[3];
+    int keyCode = -1;
+    sscanf(buf, "%2s %d\n", &keyType, &keyCode);
+    assert(keyCode >= 0);
+    if(strcmp(keyType, "kd") == 0){
+      printf("[miniSDL-event] KEYDOWN, keycode = %d\n", keyCode);
+      event -> type = SDL_KEYDOWN;
+      event -> key.keysym.sym = keyCode;
+    }else if(strcmp(keyType, "ku") == 0){
+      printf("[miniSDL-event] KEY UP , keycode = %d\n", keyCode);
+      event -> type = SDL_KEYUP;
+      event -> key.keysym.sym = keyCode;
+    }else{
+      assert(0); // should not reach here!
+    }
+  }
   return 1;
 }
 
