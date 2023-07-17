aca71d2ffc8bea0ebe8d9106f57947aa92b9561a
Jasonyanyusong <jasonyys@uw.edu>
2023-07-17 15:38:01 +0800

    navy-apps: SDLevent implement PollEvent

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -15,7 +15,33 @@ int SDL_PushEvent(SDL_Event *ev) {
 }
 
 int SDL_PollEvent(SDL_Event *ev) {
-  return 0;
+  char buf[64];
+  int retNDL_PollEvent = -1;
+  retNDL_PollEvent = NDL_PollEvent(buf, sizeof(buf));
+  assert(retNDL_PollEvent >= 0);
+  //printf("[miniSDL-event: PollEvent] NDL_PollEvent get %d\n", retNDL_PollEvent);
+  if(retNDL_PollEvent == 0){
+    return 0;
+  }else{
+    //printf("[miniSDL-event: PollEvent] receive event: %s\n", buf);
+    char keyType[3];
+    int keyCode= -1;
+    sscanf(buf, "%2s %d\n", &keyType, &keyCode);
+    assert(keyCode >= 0);
+    if(strcmp(keyType, "kd") == 0){
+      //printf("[miniSDL-event: PollEvent] KEYDOWN, keycode = %d\n", keyCode);
+      ev -> type = SDL_KEYDOWN;
+      ev -> key.keysym.sym = keyCode;
+    }else if(strcmp(keyType, "ku") == 0){
+      //printf("[miniSDL-event: PollEvent] KEY UP , keycode = %d\n", keyCode);
+      ev -> type = SDL_KEYUP;
+      ev -> key.keysym.sym = keyCode;
+    }else{
+      assert(0); // should not reach here!
+    }
+    return 1;
+  }
+  return -1;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
