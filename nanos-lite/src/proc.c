ea4a594c32af9af0a497190b7c167af3da2bf3c0
Jasonyanyusong <jasonyys@uw.edu>
2023-07-19 16:56:45 +0800

    nanos: switch proc to pal

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,8 @@ void init_proc() {
 
   // load program here
 
-  naive_uload(NULL, "/bin/bird");
+  naive_uload(NULL, "/bin/pal");
+  //naive_uload(NULL, "/bin/bird");
   //naive_uload(NULL, "/bin/nterm");
   //naive_uload(NULL, "/bin/menu");
   //naive_uload(NULL, "/bin/nslider");
