74eb908368aa6815eb46d596d88bab1df01bece8
Jasonyanyusong <jasonyys@uw.edu>
2023-07-20 11:35:25 +0800

    ysyx: folder npca -> npc

--- npcb/src/test/scala/npcbtest.scala
+++
@@ -1,6 +0,0 @@
-package npcb
-import chisel3.stage.ChiselGeneratorAnnotation
-
-object npcMain extends App{
-    (new chisel3.stage.ChiselStage).execute(Array("--target-dir", "generated/npc"), Seq(ChiselGeneratorAnnotation(() => new npc)))
-}
