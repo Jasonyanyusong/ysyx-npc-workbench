ab3de70c3d6a946144159a313a7a229c80702729
Jasonyanyusong <jasonyys@uw.edu>
2023-07-27 21:20:54 +0800

    npc: new opcode collection implemented

--- npc/src/main/scala/helper/opcode.scala
+++ npc/src/main/scala/helper/opcode.scala
@@ -17,20 +17,57 @@
 package npc.helper.opcode
 
 object OpEXU{
-    // TODO: Add more operation codes
-    def EX_NOP  = 0.U(7.W)
-    def EX_PS1  = 1.U(7.W)
-    def EX_ADD  = 2.U(7.W)
-    def EX_SLT  = 3.U(7.W)
-    def EX_SLTU = 4.U(7.W)
-    def EX_XOR  = 5.U(7.W)
-    def EX_OR   = 6.U(7.W)
+    // TODO: Add more operation codes, need check!
+    def EX_NOP    =  0.U(7.W)
+    def EX_PS1    =  1.U(7.W)
+    def EX_ADD    =  2.U(7.W)
+    def EX_SLT    =  3.U(7.W)
+    def EX_SLTU   =  4.U(7.W)
+    def EX_XOR    =  5.U(7.W)
+    def EX_OR     =  6.U(7.W)
+    def EX_AND    =  7.U(7.W)
+    def EX_SLL    =  8.U(7.W)
+    def EX_SRL    =  9.U(7.W)
+    def EX_SRA    = 10.U(7.W)
+    def EX_SUB    = 11.U(7.W)
+    def EX_ADDW   = 12.U(7.W)
+    def EX_SLLW   = 13.U(7.W)
+    def EX_SRLW   = 14.U(7.W)
+    def EX_SRAW   = 15.U(7.W)
+    def EX_MUL    = 16.U(7.W)
+    def EX_MULH   = 17.U(7.W)
+    def EX_MULHSU = 18.U(7.W)
+    def EX_MULHU  = 19.U(7.W)
+    def EX_DIV    = 20.U(7.W)
+    def EX_DIVU   = 21.U(7.W)
+    def EX_REM    = 22.U(7.W)
+    def EX_REMU   = 23.U(7.W)
+    def EX_MULW   = 24.U(7.W)
+    def EX_DIVW   = 25.U(7.W)
+    def EX_DIVUW  = 26.U(7.W)
+    def EX_REMW   = 27.U(7.W)
+    def EX_REMUW  = 28.U(7.W)
 }
 
-object OpLSU{
+object OpLSULen{
     // TODO: Add more operation codes
+    def LS_B = 0.U(2.W)
+    def LS_H = 1.U(2.W)
+    def LS_W = 2.U(2.W)
+    def LS_D = 3.U(2.W)
+}
+
+object OpLSUFunc{
+    // TODO: Add more operation codes
+    def LS_NOP = 0.U(2.W)
+    def LS_LD  = 1.U(2.W)
+    def LS_ST  = 2.U(2.W)
 }
 
 object OpWBU{
     // TODO: Add more operation codes
+    def WB_NOP  = 0.U(2.W)
+    def WB_EXU  = 1.U(2.W)
+    def WB_LSU  = 2.U(2.W)
+    def WB_SNPC = 3.U(2.W)
 }
