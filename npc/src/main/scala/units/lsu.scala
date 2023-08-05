db62ecbf0432716bb2f3f4ec8e6f7fe2108dff83
Jasonyanyusong <jasonyys@uw.edu>
2023-08-05 16:01:15 +0800

    npc: add LSU's header

---
+++ npc/src/main/scala/units/lsu.scala
@@ -0,0 +1,23 @@
+/***************************************************************************************
+* Copyright (c) 2023 Yusong Yan, Beijing 101 High School
+* Copyright (c) 2023 Yusong Yan, University of Washington - Seattle
+*
+* YSYX-NPC is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+package npc.units
+
+import chisel3._
+import chisel3.util._
+
+import npc.helper.defs.Base._
+import npc.helper.opcode.OpLSU._
