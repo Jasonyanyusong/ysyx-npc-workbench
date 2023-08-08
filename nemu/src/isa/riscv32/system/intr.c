537beb1b1dd9ccf960c84a1ff0d672a15b93f3a3
Jasonyanyusong <jasonyys@uw.edu>
2023-08-08 14:00:20 +0800

    nemu: RV32IM add insts, CSR

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -15,10 +15,27 @@
 
 #include <isa.h>
 
+#define CSR_MSTATUS 0x300
+#define CSR_MTVEC   0x305
+#define CSR_MEPC    0x341
+#define CSR_MCAUSE  0x342
+
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+#ifdef CONFIG_RV_Privileged
+  IFDEF(CONFIG_ShowLogForInterrupt, Log("CPU raised interupt at pc 0x%lx with interrupt NO.0x%lx, mevtc = 0x%lx, gpr[17] = 0x%lx", epc, NO, cpu.csr[CSR_MTVEC], cpu.gpr[17]));
+  cpu.csr[CSR_MSTATUS] = 0x0;
+  cpu.csr[CSR_MEPC] = epc;
+  cpu.csr[CSR_MCAUSE] = NO;
+
+#ifdef CONFIG_ErrorTrace
+  etrace_write(cpu.csr[CSR_MSTATUS], cpu.csr[CSR_MTVEC], cpu.csr[CSR_MEPC], cpu.csr[CSR_MCAUSE]);
+#endif
+
+  return cpu.csr[CSR_MTVEC];
+#endif
 
   return 0;
 }
