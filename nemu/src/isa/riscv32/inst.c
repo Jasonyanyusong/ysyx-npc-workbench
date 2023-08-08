2c0cdaf5ba4c9862f12fb75105253902ad8900ed
Jasonyanyusong <jasonyys@uw.edu>
2023-08-08 13:20:16 +0800

    nemu: add prepare for RV32IM

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -19,11 +19,12 @@
 #include <cpu/decode.h>
 
 #define R(i) gpr(i)
+#define C(i) csr(i)
 #define Mr vaddr_read
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
+  TYPE_R, TYPE_I, TYPE_U, TYPE_S, TYPE_B, TYPE_J,
   TYPE_N, // none
 };
 
@@ -32,6 +33,8 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immB() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 12) | BITS(i, 7, 7) << 11 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1;} while(0)
+#define immJ() do { *imm = (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 19, 12) << 12 | BITS(i, 20, 20) << 11 | BITS(i, 30, 21) << 1;} while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
@@ -39,9 +42,12 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
   int rs2 = BITS(i, 24, 20);
   *rd     = BITS(i, 11, 7);
   switch (type) {
+    case TYPE_R: src1R(); src2R();         break;
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
+    case TYPE_J:                   immJ(); break;
   }
 }
 
@@ -50,11 +56,32 @@ static int decode_exec(Decode *s) {
   word_t src1 = 0, src2 = 0, imm = 0;
   s->dnpc = s->snpc;
 
+#ifdef CONFIG_ShowInstInfo
+  char instruction_bin_string[33] = {0};
+  u_int32_t get_instruction = s->isa.inst.val;
+  for(int i = 0; i <= 31; i = i + 1)
+  {
+    if(get_instruction >= pow(2, 31 - i))
+    {
+      instruction_bin_string[i] = '1';
+      get_instruction = get_instruction - pow(2, 31 - i);
+    }
+    else
+    {
+      instruction_bin_string[i] = '0';
+    }
+  }
+  instruction_bin_string[32] = '\0';
+#endif
+
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* execute body */ ) { \
   decode_operand(s, &rd, &src1, &src2, &imm, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
 }
+
+  int rs1 = BITS(s->isa.inst.val, 19, 15);
+  int rs2 = BITS(s->isa.inst.val, 24, 20);
 
   INSTPAT_START();
   INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui    , U, R(rd) = imm);
@@ -67,10 +94,20 @@ static int decode_exec(Decode *s) {
 
   R(0) = 0; // reset $zero to 0
 
+#ifdef CONFIG_ShowInstInfo
+  printf("Inst: %s (0x%8x)\n", instruction_bin_string, s->isa.inst.val);
+  printf("rs1 = 0x%x (%d), rs2 = 0x%x (%d), rd = 0x%x (%d)\n", rs1, rs1, rs2, rs2, rd, rd);
+  printf("src1 = 0x%8lx (%ld), src2 = 0x%8lx (%ld), R(rd) = 0x%8lx (%ld), imm = 0x%8lx (%ld)\n", src1, src1, src2, src2, R(rd), R(rd), imm, imm);
+  printf("pc = 0x%lx, dnpc = 0x%lx, snpc = 0x%lx\n", s -> pc, s -> dnpc, s -> snpc);
+#endif
+
   return 0;
 }
 
 int isa_exec_once(Decode *s) {
+#ifdef CONFIG_ShowInstInfo
+  printf("********************************************************************************************************************************\n");
+#endif
   s->isa.inst.val = inst_fetch(&s->snpc, 4);
   return decode_exec(s);
 }
