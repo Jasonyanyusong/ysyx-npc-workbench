d4ec92244eec7e598f853f99b64c720a83a1153f
Jasonyanyusong <jasonyys@uw.edu>
2023-08-09 13:18:36 +0800

    npc: add decode enable to judge IDU valid

--- npc/src/main/scala/units/iDecode/decoder.scala
+++ npc/src/main/scala/units/iDecode/decoder.scala
@@ -342,7 +342,7 @@ class IDU extends Module{
     ioInternal.oPC := ioInternal.iPC
 
     // Connect Pipline Signals
-    ioInternal.oMasterValid := IDU_NotBusy.asBool
+    ioInternal.oMasterValid := (IDU_NotBusy.asBool && iDecodeEnable.asBool)
     ioInternal.oSlaveReady := (IDU_NotBusy.asBool && ioInternal.iMasterReady)
 
     when(ioInternal.iMasterReady.asBool){
