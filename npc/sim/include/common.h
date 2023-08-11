99784bd145691a752bd63e0bea44a549d0d79408
Jasonyanyusong <jasonyys@uw.edu>
2023-08-11 09:46:04 +0800

    npc-sim: implement keyboard

--- npc/sim/include/common.h
+++ npc/sim/include/common.h
@@ -31,3 +31,11 @@ typedef uint64_t word_t;
 #define ARRLEN(arr) (int)(sizeof(arr) / sizeof(arr[0]))
 
 enum { NPC_RUNNING, NPC_STOP, NPC_END, NPC_ABORT, NPC_QUIT };
+
+typedef struct {
+  int state;
+  word_t halt_pc;
+  uint32_t halt_ret;
+} NPCState;
+
+extern NPCState npc_state;
