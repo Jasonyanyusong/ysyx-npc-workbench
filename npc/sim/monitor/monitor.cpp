56c24a01f67d7af596ba7b78e49cb69ca3977630
Jasonyanyusong <jasonyys@uw.edu>
2023-08-14 10:59:50 +0800

    npc-sim: remove a finished TODO

--- npc/sim/monitor/monitor.cpp
+++ npc/sim/monitor/monitor.cpp
@@ -22,7 +22,6 @@ char* image_path = NULL;
 char* diff_so_file = NULL;
 
 uint32_t default_img[] = {
-    // TODO: write binary of default image
     // need to support both RV32 and RV64
     0x800002b7, // lui t0, 0x80000
     0x0002a023, // sw zero ,0(t0)
