2e0bb4573e6a9f92d155edabb66d52ccab34526d
Jasonyanyusong <jasonyys@uw.edu>
2023-08-28 14:02:47 +0800

    nemu: difftest add log for indicating PC

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -60,6 +60,7 @@ void difftest_regcpy(void *dut, bool direction) {
 
 void difftest_exec(uint64_t n) {
   cpu_exec(n);
+  Log("Use NEMU as ref model, CPU execute at PC = 0x%x", cpu.pc);
 }
 
 void difftest_raise_intr(word_t NO) {
