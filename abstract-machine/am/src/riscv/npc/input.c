1acb093cec635c296899e364b43374466d3e198a
Jasonyanyusong <jasonyys@uw.edu>
2023-08-29 10:52:52 +0800

    am: riscv-npc implement keyboard

--- abstract-machine/am/src/riscv/npc/input.c
+++ abstract-machine/am/src/riscv/npc/input.c
@@ -1,6 +1,13 @@
 #include <am.h>
+#include "riscv/riscv.h"
+
+#define AM_RV_NPC_KBD_ADDR 0xa0000060
+
+#define KEYDOWN_MASK 0x8000
+#define KEYCODE_MASK 0x7FFF
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  uint32_t keyborad_data = inl(AM_RV_NPC_KBD_ADDR);
+  kbd->keydown = keyborad_data & KEYDOWN_MASK;
+  kbd->keycode = keyborad_data & KEYCODE_MASK;
 }
