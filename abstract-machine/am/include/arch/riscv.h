956d923217279df494df418b5bf2c2c8a5262386
Jasonyanyusong <jasonyys@uw.edu>
2023-08-29 16:18:54 +0800

    am: riscv fix registers order

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -1,9 +1,16 @@
 #ifndef ARCH_H__
 #define ARCH_H__
 
+#ifdef __riscv_e
+#define NUMBER_GPR 16
+#else
+#define NUMBER_GPR 32
+#endif
+
 struct Context {
   // TODO: fix the order of these members to match trap.S
-  uintptr_t mepc, mcause, gpr[32], mstatus;
+  //uintptr_t mepc, mcause, gpr[32], mstatus;
+  uintptr_t gpr[NUMBER_GPR], mcause, mstatus, mepc;
   void *pdir;
 };
 
@@ -13,9 +20,9 @@ struct Context {
 #define GPR1 gpr[17] // a7
 #endif
 
-#define GPR2 gpr[0]
-#define GPR3 gpr[0]
-#define GPR4 gpr[0]
-#define GPRx gpr[0]
+#define GPR2 gpr[10]
+#define GPR3 gpr[11]
+#define GPR4 gpr[12]
+#define GPRx gpr[10]
 
 #endif
