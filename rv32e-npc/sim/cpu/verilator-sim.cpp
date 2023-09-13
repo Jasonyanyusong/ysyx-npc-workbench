70b06b51667d350200545fa2430789a917f2daed
Jasonyanyusong <jasonyys@uw.edu>
2023-09-13 10:31:17 +0800

    rv32e-npc-sim: add config for runtime-msgs

--- rv32e-npc/sim/cpu/verilator-sim.cpp
+++ rv32e-npc/sim/cpu/verilator-sim.cpp
@@ -31,6 +31,8 @@ word_t last_diff_pc = 0;
 #define NPC_RUNNING 0
 #define NPC_STOPPED 1
 
+//#define CONFIG_RUNTIME_MESSAGE
+
 int memory_delay = 0;
 
 void pmem_write(word_t, int, word_t);
@@ -117,12 +119,17 @@ void sim_one_cycle(){
     get_regs(); // used as print registers or difftest
 
     if(top -> ioNPCDebug_Worked){
+        #ifdef CONFIG_RUNTIME_MESSAGE
         printf("[verilator-sim : sim_one_cycle] NPC commited an instruction at pc = 0x%x\n", top -> ioNPCDebug_PC_COMMIT);
+        #endif
     }
 
     #ifdef CONFIG_DIFFTEST
     if(top -> ioNPCDebug_Worked && top -> ioNPCDebug_PC_COMMIT != last_diff_pc){
+        #ifdef CONFIG_RUNTIME_MESSAGE
         printf("[verilator-sim : sim_one_cycle] WBU indicated it worked, so do a difftest at pc = 0x%x\n", cpu.pc);
+        #endif
+
         difftest_one_exec();
         if(!difftest_check_reg()){
             npc_state.state = NPC_ABORT;
@@ -145,7 +152,9 @@ void sim_one_cycle(){
         #endif
     }
 
+    #ifdef CONFIG_RUNTIME_MESSAGE
     printf("\n\n");
+    #endif
     
     return;
 }
@@ -181,31 +190,49 @@ double compute_ipc(){
 #define MEM_WRITE 0b10
 
 void sim_mem(int delay_cycle){
-    //printf("[verilator-sim : sim-mem] at cycle %d\n", cycle);
+    #ifdef CONFIG_RUNTIME_MESSAGE
+    printf("[verilator-sim : sim-mem] at cycle %d\n", cycle);
+    #endif
 
     assert(top);
 
     word_t LS_MemAddr = (uint32_t)top -> ioNPC_iLoadStore_oMemoryAddr;
     int LS_MemLen = (int)pow(2, top -> ioNPC_iLoadStore_oMemoryLen);
+
+    #ifdef CONFIG_RUNTIME_MESSAGE
     printf("[verilator-sim : sim_mem] LS_MemAddr is 0x%x, len = %d, op = %d\n", LS_MemAddr, LS_MemLen, top -> ioNPC_iLoadStore_oMemoryOP);
+    #endif
 
     assert(LS_MemLen >= 1 && LS_MemLen <= 8);
     
     switch(top -> ioNPC_iLoadStore_oMemoryOP){
         case(MEM_NOP):{
+            #ifdef CONFIG_RUNTIME_MESSAGE
             printf("[verilator-sim : sim_mem] LS do not access mem here\n");
+            #endif
+
             top -> ioNPC_iLoadStore_iMemoryRead = 0;
             break;
         }
         case(MEM_READ):{
+            #ifdef CONFIG_RUNTIME_MESSAGE
             printf("[verilator-sim : sim_mem] LS do read mem here");
+            #endif
+
             word_t ret = pmem_read(LS_MemAddr, LS_MemLen);
             top -> ioNPC_iLoadStore_iMemoryRead = ret;
+
+            #ifdef CONFIG_RUNTIME_MESSAGE
             printf(", data is 0x%lx\n", ret);
+            #endif
+
             break;
         }
         case(MEM_WRITE):{
+            #ifdef CONFIG_RUNTIME_MESSAGE
             printf("[verilator-sim : sim_mem] LS do write mem here, data is 0x%lx\n", top -> ioNPC_iLoadStore_oMemoryWrite);
+            #endif
+
             top -> ioNPC_iLoadStore_iMemoryRead = 0;
             pmem_write(LS_MemAddr, LS_MemLen, top -> ioNPC_iLoadStore_oMemoryWrite);
             break;
@@ -216,13 +243,22 @@ void sim_mem(int delay_cycle){
     top -> eval();
 
     word_t IF_MemAddr = (uint32_t)top -> ioNPC_iFetch_oPC;
+
+    #ifdef CONFIG_RUNTIME_MESSAGE
     printf("[verilator-sim : sim_mem] IF_MemAddr is 0x%x\n", IF_MemAddr);
+    #endif
 
     if(top -> ioNPC_iFetch_oMemEnable){
+        #ifdef CONFIG_RUNTIME_MESSAGE
         printf("[verilator-sim] at cycle %d, get iFetch Memory request, addr 0x%x, inst 0x%x\n", cycle, IF_MemAddr, pmem_read(IF_MemAddr, 4));
+        #endif
+
         top -> ioNPC_iFetch_iInst = pmem_read(IF_MemAddr, 4);
     }else{
+        #ifdef CONFIG_RUNTIME_MESSAGE
         printf("[verilator-sim] at cycle %d, no iFetch Memory request\n", cycle);
+        #endif
+
         top -> ioNPC_iFetch_iInst = 0;
     }
 
