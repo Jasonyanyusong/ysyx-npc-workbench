82f94c407c9dccab86353721ce4fbf6025e21a80
Jasonyanyusong <jasonyys@uw.edu>
2023-12-13 16:07:30 -0800

    write program display bj101 name

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,8 +28,10 @@ void init_proc() {
 
   // load program here
 
+  naive_uload(NULL, "/bin/bj101-name");
+
   //naive_uload(NULL, "/bin/pal");
-  naive_uload(NULL, "/bin/bird");
+  //naive_uload(NULL, "/bin/bird");
   //naive_uload(NULL, "/bin/nterm");
   //naive_uload(NULL, "/bin/menu");
   //naive_uload(NULL, "/bin/nslider");
