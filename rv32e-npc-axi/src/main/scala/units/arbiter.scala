3c98cd39ac3cdcbcb400986e7c2dd89879531dfd
Jasonyanyusong <jasonyys@uw.edu>
2023-12-21 19:23:39 -0800

    arbiter implement mode sel

--- rv32e-npc-axi/src/main/scala/units/arbiter.scala
+++ rv32e-npc-axi/src/main/scala/units/arbiter.scala
@@ -36,6 +36,29 @@ class Arbiter extends Module {
     val lsu_ar_b = IO(Flipped(new AXIMasterAR))
     val lsu_r_b  = IO(Flipped(new AXIMasterR))
 
+    // Output channels
+    val out_ar_b = IO(new AXIMasterAR)
+    val out_r_b  = IO(new AXIMasterR)
+
     // Arbiter mode
     val mode = RegInit(ArbiterMode.ArbiterFree)
+
+    // Master requesting signal?
+    val ifu_request = ifu_ar_b.oMasterARvalid
+    val lsu_request = lsu_ar_b.oMasterARvalid
+
+    // Check and update status when arbiter is
+    // in free mode, otherwise don't change
+    mode := Mux(mode === ArbiterMode.ArbiterFree, MuxCase(
+        mode, Array(
+            (ifu_request === false.B && lsu_request === false.B) -> (ArbiterMode.ArbiterFree),
+            (ifu_request === false.B && lsu_request === true.B ) -> (ArbiterMode.ArbiterLSOn),
+            (ifu_request === true.B  && lsu_request === false.B) -> (ArbiterMode.ArbiterIFOn),
+            (ifu_request === true.B  && lsu_request === true.B ) -> (ArbiterMode.ArbiterLSOn)
+        )
+    ), mode) // don't change the mode if it's not free
+
+    // TODO: connect datapath according to arbiter's mode
+
+    // TODO: update mode according to read response
 }
