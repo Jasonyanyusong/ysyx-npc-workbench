40ce4637de32250094659b8acef36acdbc374fe5
Jasonyanyusong <jasonyys@uw.edu>
2023-12-29 22:43:43 -0800

    lsu + axi

--- rv32e-npc-axi/src/main/scala/units/lsu.scala
+++ rv32e-npc-axi/src/main/scala/units/lsu.scala
@@ -43,10 +43,11 @@ class iLoadStoreInternal extends Bundle{
 }
 
 object LSU_AXI_State {
-    val AXI_Free = 0.U(2.W)
-    val AXI_Sending = 1.U(2.W)
-    val AXI_Reading = 2.U(2.W)
-    val AXI_Writing = 3.U(2.W)
+    val AXI_Free = 0.U(3.W)
+    val AXI_RSending = 1.U(3.W)
+    val AXI_RReading = 2.U(3.W)
+    val AXI_WSending = 3.U(3.W)
+    val AXI_WWriting = 4.U(3.W)
 }
 
 object LSU_AXI_R_Defs {
@@ -61,19 +62,19 @@ object LSU_AXI_W_Defs {
     val AXI_AW_BURST = 0.U(2.W)
 }
 
-object LSU_AxSIZE {
+/*object LSU_AxSIZE {
     val AxSIZE_BYTE = 0.U(3.W) // lb lbu sb
     val AxSIZE_HALF = 1.U(3.W) // lh lhu sh
     val AxSIZE_WORD = 2.U(3.W) // lw sw
-}
+}*/
 
-class iLoadStoreExternal extends Bundle{
+/*class iLoadStoreExternal extends Bundle{
     val oMemoryOP = Output(UInt(2.W))
     val oMemoryLen = Output(UInt(2.W))
     val oMemoryAddr = Output(UInt(AddrWidth.W))
     val oMemoryWrite = Output(UInt(DataWidth.W))
     val iMemoryRead = Input(UInt(DataWidth.W))
-}
+}*/
 
 class iLoadStoreDebug extends Bundle{
     val oLoadStoreTaken = Output(Bool())
@@ -82,7 +83,7 @@ class iLoadStoreDebug extends Bundle{
 
 class LSU extends Module{
     val ioInternal = IO(new iLoadStoreInternal)
-    val ioExternal = IO(new iLoadStoreExternal)
+    //val ioExternal = IO(new iLoadStoreExternal)
 
     // AXI
     val LSU_AXI_AW = IO(new AXIMasterAW)
@@ -90,6 +91,16 @@ class LSU extends Module{
     val LSU_AXI_B  = IO(new AXIMasterB)
     val LSU_AXI_AR = IO(new AXIMasterAR)
     val LSU_AXI_R  = IO(new AXIMasterR)
+
+    // axi read hard wired settings
+    LSU_AXI_AR.oMasterARid := LSU_AXI_R_Defs.AXI_AR_ID
+    LSU_AXI_AR.oMasterARlen := LSU_AXI_R_Defs.AXI_AR_LEN
+    LSU_AXI_AR.oMasterARburst := LSU_AXI_R_Defs.AXI_AR_BURST
+
+    // axi write hard wired settings
+    LSU_AXI_AW.oMasterAWid := LSU_AXI_W_Defs.AXI_AW_ID
+    LSU_AXI_AW.oMasterAWlen := LSU_AXI_W_Defs.AXI_AW_LEN
+    LSU_AXI_AW.oMasterAWburst := LSU_AXI_W_Defs.AXI_AW_BURST
 
     val ioDebug    = IO(new iLoadStoreDebug)
 
@@ -112,7 +123,11 @@ class LSU extends Module{
 
     val LSU_StateOK = (ioInternal.iSlaveValid.asBool && ioInternal.iMasterReady.asBool)
 
-    val LSU_LEN = Mux(LSU_StateOK, DecodeBundle(6, 5), LS_B)
+    // send address to both channels
+    LSU_AXI_AR.oMasterARaddr := Mux(LSU_StateOK, EXU_RET, 0.U(DataWidth.W))
+    LSU_AXI_AW.oMasterARaddr := Mux(LSU_StateOK, EXU_RET, 0.U(DataWidth.W))
+
+    //val LSU_LEN = Mux(LSU_StateOK, DecodeBundle(6, 5), LS_B)
     val LSU_FUNC = Mux(LSU_StateOK, DecodeBundle(4, 3), LS_NOP)
     val LSU_ADDR = Mux(LSU_StateOK, EXU_RET, 0.U(DataWidth.W))
     val LSU_MEMOP = Mux(LSU_StateOK, MuxCase(MEM_NOP, Array(
@@ -123,8 +138,42 @@ class LSU extends Module{
     )
     val LSU_WRITE_SRC = Mux(LSU_StateOK, LSU_SRC, 0.U(DataWidth.W))
 
+    // send the size signal to aw and ar
+    val LSU_SIZE = Mux(LSU_StateOK, DecodeBundle(6, 5), LS_B)
+    LSU_AXI_AR.oMasterARsize := LSU_SIZE
+    LSU_AXI_AW.oMasterARsize := LSU_SIZE
+
+    // deterimine which axi channel to be used (read/write)
+    val AXI_MEMOP := MuxCase(LS_NOP, Array(
+        (LSU_ADDR >= "h80000000".U && LSU_ADDR <= "h8fffffff".U) -> LSU_MEMOP,
+        (LSU_ADDR >= "ha0000048".U && LSU_ADDR <= "ha000004f".U) -> LSU_MEMOP,
+        (LSU_ADDR >= "ha00003f8".U && LSU_ADDR <= "ha00003ff".U) -> LSU_MEMOP,
+        (LSU_ADDR >= "ha0000060".U && LSU_ADDR <= "ha0000063".U) -> LSU_MEMOP,
+    ))
+
+    AXI_State_LSU := MuxCase(AXI_State_LSU, Array(
+        // if lsu's axi state is free, it may change to send request to r or w
+        (AXI_State_LSU === LSU_AXI_State.AXI_Free) -> (MuxCase(AXI_Free, Array(
+            (AXI_MEMOP === LS_NOP)    -> (LSU_AXI_State.AXI_Free),
+            (AXI_MEMOP === MEM_READ)  -> (LSU_AXI_State.AXI_RSending),
+            (AXI_MEMOP === MEM_WRITE) -> (LSU_AXI_State.AXI_WSending),
+        ))),
+
+        // if lsu's axi state is sending signal, see if the axi is ready
+        (AXI_State_LSU === LSU_AXI_State.AXI_RSending) -> (Mux(
+            LSU_AXI_AR.iMasterARready, LSU_AXI_State.AXI_RReading, AXI_State_LSU)),
+        (AXI_State_LSU === LSU_AXI_State.AXI_WSending) -> (Mux(
+            LSU_AXI_AW.iMasterARready, LSU_AXI_State.AXI_WWriting, AXI_State_LSU)),
+
+        // if lsu's axi state is waiting for response, see if axi is done
+        (AXI_State_LSU === LSU_AXI_State.AXI_RReading) -> (Mux(
+            LSU_AXI_R.iMasterRvalid, LSU_AXI_State.AXI_Free, AXI_State_LSU)),
+        (AXI_State_LSU === LSU_AXI_State.AXI_WWriting) -> (Mux(
+            LSU_AXI_B.iMasterBvalid, LSU_AXI_State.AXI_Free, AXI_State_LSU)),
+    ))
+
     // Connect IO External
-    ioExternal.oMemoryOP := MuxCase(LS_NOP, Array(
+    /*ioExternal.oMemoryOP := MuxCase(LS_NOP, Array(
         (LSU_ADDR >= "h80000000".U && LSU_ADDR <= "h8fffffff".U) -> LSU_MEMOP,
         (LSU_ADDR >= "ha0000048".U && LSU_ADDR <= "ha000004f".U) -> LSU_MEMOP,
         (LSU_ADDR >= "ha00003f8".U && LSU_ADDR <= "ha00003ff".U) -> LSU_MEMOP,
@@ -132,9 +181,9 @@ class LSU extends Module{
     ))
     ioExternal.oMemoryAddr := LSU_ADDR
     ioExternal.oMemoryWrite := LSU_WRITE_SRC
-    ioExternal.oMemoryLen := LSU_LEN
+    ioExternal.oMemoryLen := LSU_LEN*/
 
-    val MEM_READ_RET = Mux(LSU_StateOK, ioExternal.iMemoryRead, 0.U(DataWidth.W))
+    val MEM_READ_RET = Mux(LSU_StateOK, LSU_AXI_R.iMasterRdata(31, 0), 0.U(DataWidth.W))
 
     val LSU_Compute_Result = Mux(LSU_StateOK, MuxCase(0.U(DataWidth.W), Array(
             (LSU_FUNC === LS_LD) -> (MuxCase(0.U(DataWidth.W), Array(
@@ -159,6 +208,6 @@ class LSU extends Module{
     ioInternal.PipeLine_LS2WB_ChangeReg := LSU_StateOK && LSU_NotBusy
 
     // Pipeline shake hand implementation
-    ioInternal.oSlaveReady := (LSU_NotBusy.asBool && ioInternal.iMasterReady.asBool)
-    ioInternal.oMasterValid := (LSU_NotBusy.asBool && ioInternal.iSlaveValid.asBool)
+    ioInternal.oSlaveReady := (AXI_State_LSU === LSU_AXI_State.AXI_Free) && (LSU_NotBusy.asBool && ioInternal.iMasterReady.asBool)
+    ioInternal.oMasterValid := (AXI_State_LSU === LSU_AXI_State.AXI_Free) && (LSU_NotBusy.asBool && ioInternal.iSlaveValid.asBool)
 }
