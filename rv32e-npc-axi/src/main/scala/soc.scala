359026d7d2b1eff96442d1f6f835310496c7c4a9
Jasonyanyusong <jasonyys@uw.edu>
2024-01-07 10:51:25 -0800

    sram implementation

--- rv32e-npc-axi/src/main/scala/soc.scala
+++ rv32e-npc-axi/src/main/scala/soc.scala
@@ -23,11 +23,13 @@ import npc._
 
 class SOCIO extends Bundle {
     // Memory Write Channel
+    val sram_w_en = Output(Bool())
     val sram_waddr_o = Output(UInt(32.W))
     val sram_wdata_o = Output(UInt(32.W))
     val sram_wsize_o = Output(UInt(2.W))
 
     // Memory Read Channel
+    val sram_r_en = Output(Bool())
     val sram_raddr_o = Output(UInt(32.W))
     val sram_rdata_i = Input (UInt(32.W))
     val sram_rsize_o = Output(UInt(2.W))
@@ -40,5 +42,28 @@ class SOC extends Module {
     // Processor core
     val core = Module(new NPC)
 
+    // AXI delay
+    val AXI_DELAY = 5
+
+    // Write manipulation
+    core.NPC_AXI_AW.iMasterAWready := true.B
+    core.NPC_AXI_W.iMasterWready := true.B
+    core.NPC_AXI_B.iMasterBvalid := ShiftRegister(core.NPC_AXI_AW.oMasterAWvalid && core.NPC_AXI_W.oMasterWvalid, AXI_DELAY)
+    core.NPC_AXI_B.iMasterBresp := 0.U(2.W)
+    core.NPC_AXI_B.iMasterBid := 2.U(4.W)
+
+    io.sram_r_en := core.NPC_AXI_AW.oMasterAWvalid && core.NPC_AXI_W.oMasterWvalid
+    io.sram_wdata_o := core.NPC_AXI_W.oMasterWdata
+    io.sram_waddr_o := core.NPC_AXI_AW.oMasterAWaddr
+    io.sram_wsize_o := core.NPC_AXI_AW.oMasterAWsize
+
+    // read manipulation
+    core.NPC_AXI_AR.iMasterARready := true.B
+    core.NPC_AXI_R.iMasterRvalid := ShiftRegister(core.NPC_AXI_AR.oMasterARvalid, AXI_DELAY)
+    core.NPC_AXI_R.iMasterRresp := 0.U(2.W)
+    core.NPC_AXI_R.iMasterRdata := ShiftRegister(Cat(Fill(32, 0), io.sram_rdata_i), AXI_DELAY)
+        
+    core.NPC_AXI_R.iMasterRlast := true.B
+    core.NPC_AXI_R.iMasterRid := ShiftRegister(core.NPC_AXI_AR.oMasterARid, AXI_DELAY)
     
 }
